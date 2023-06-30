b171fd1f39c007c1ee9e0bc812da7dc3e7ff0a8a
meryemessaiydy <maryamessaiydy@gmail.com>
2023-06-30 12:17:03 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - Entry point 
+ * main - A program that print a line using the printf function 
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 { 
