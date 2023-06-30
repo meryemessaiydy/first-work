16ffa13f7679478660edb662f6fd3fdd40499d73
meryemessaiydy <maryamessaiydy@gmail.com>
2023-06-30 08:57:28 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ * main - A program that prints a line withe puts function
+ * Return: 0 (success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
